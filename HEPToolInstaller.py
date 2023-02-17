6016cab232654727dba9f08142ac94b6bf79cd80
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2023-02-17 16:35:49 +0100

    updating py8 version

--- HEPToolInstaller.py
+++ HEPToolInstaller.py
@@ -147,7 +147,7 @@ _HepTools = {'hepmc':
                 'install_path':  '%(prefix)s/rosetta/'},
              'pythia8':
                {'install_mode':'Default',
-                'version':       '8306',
+                'version':       '8309',
                 'www': 'https://pythia.org/download/pythia83',
 # Official version
                 'tarball':      ['online','%(www)s/pythia%(version)s.tgz'],
