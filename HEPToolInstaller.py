1771ca3540002a31b719e9b7a8eab42eb8d8c2a9
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2023-02-17 17:07:16 +0100

    revert to py306

--- HEPToolInstaller.py
+++ HEPToolInstaller.py
@@ -147,7 +147,7 @@ _HepTools = {'hepmc':
                 'install_path':  '%(prefix)s/rosetta/'},
              'pythia8':
                {'install_mode':'Default',
-                'version':       '8309',
+                'version':       '8306',
                 'www': 'https://pythia.org/download/pythia83',
 # Official version
                 'tarball':      ['online','%(www)s/pythia%(version)s.tgz'],
