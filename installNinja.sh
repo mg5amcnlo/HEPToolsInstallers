227ec61d371f6b898c0fe63edf24fbcaab2f5750
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2023-10-20 15:04:51 +0200

    prevent quadproc in ninja for mac

--- installNinja.sh
+++ installNinja.sh
@@ -15,7 +15,12 @@ run () {
 	echo "Entering Ninja directory and installing Ninja"
     cd Ninja
     mv VERSION TEMPVERSION
+    if [ "$(uname)" == "Darwin" ]; then
+	./configure --prefix=${NINJAINSTALLD} --enable-higher_rank --with-avholo='-L'"${ONELOOPPATH}"' -lavh_olo' FCINCLUDE=-I${ONELOOPPATH} CXX=${CXX} CXXFLAGS="${COMPILATIONFLAGS}" CPPFLAGS='-DNINJA_NO_EXCEPTIONS -fPIC' --disable-quadninja LIBS=${CPPSTANDARDLIB}
+    else
 	./configure --prefix=${NINJAINSTALLD} --enable-higher_rank --with-avholo='-L'"${ONELOOPPATH}"' -lavh_olo' FCINCLUDE=-I${ONELOOPPATH} CXX=${CXX} CXXFLAGS="${COMPILATIONFLAGS}" CPPFLAGS='-DNINJA_NO_EXCEPTIONS -fPIC' --enable-quadninja LIBS=${CPPSTANDARDLIB}
+    fi
+    
 	echo "== Content of config.log =="
 	cat config.log
 	echo "== End of content of config.log =="
