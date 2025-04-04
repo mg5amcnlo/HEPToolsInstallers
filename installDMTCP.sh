cf84dd6d780761bf25924086290d8cc594824c22
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2025-04-04 17:13:18 +0200

    add installation script

---
+++ installDMTCP.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+run () {
+
+    INSTALLD="$1"
+    VERSION="$2"
+    TARBALLPATH="$3"
+
+    mkdir -p $INSTALLD
+    cd $INSTALLD
+    ls
+    # Make sure there is only a single uncompressed folder starting with EMELA-
+    for a in `ls -d DMTCP*`; do mv $a OLD_${a}_`date '+%Y%m%d%H%M%S'`; done;
+    echo "Decompressing DMTCP ${TARBALLPATH}"
+    tar xvzf ${TARBALLPATH}
+    echo "Enter DMTCP directory and installing DMTCP"
+    ls 
+    cd dmtcp*
+    ./configure --prefix=$INSTALLD
+    make 
+    make install
+}
+
+run "$@"
